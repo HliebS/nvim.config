4a607c711afd535c8aa2713cfd9907c9c082c9b8
HliebS <h.n.solomonov@gmail.com>
2025-01-07 22:02:18 +0100

    Add mason and lspconfig

---
+++ lua/hliebs/plugins/lsp/lsp-config.lua
@@ -0,0 +1,18 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  config = function()
+    require("lspconfig").lua_ls.setup {}
+  end,
+}
--- lua/hliebs/plugins/telescope-fuzzyfinder.lua
+++ lua/hliebs/plugins/telescope-fuzzyfinder.lua
@@ -24,14 +24,13 @@ return {
       },
     })
 
-    local builtin = require("telescope.builtin")
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find under cursor" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
-    
+
   end
 }
 
