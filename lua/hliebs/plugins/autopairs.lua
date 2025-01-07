e72e6773def75171bfc2361435cb546280b5bd1e
HliebS <h.n.solomonov@gmail.com>
2025-01-07 21:23:37 +0100

    Add nvim-autopairs

---
+++ lua/hliebs/plugins/autopairs.lua
@@ -0,0 +1,16 @@
+return {
+  "windwp/nvim-autopairs",
+  event = { "InsertEnter" },
+  config = function()
+    -- import nvim-autopairs
+    local autopairs = require("nvim-autopairs")
+    --
+    -- configure autopairs
+    autopairs.setup({
+      check_ts = true, -- enable treesitter
+      ts_config = {
+        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+      },
+    })
+  end,
+}
