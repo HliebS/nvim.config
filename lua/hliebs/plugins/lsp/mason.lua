c4f2243faeb1b870dea99e6df8a518aa534d76e9
HliebS <h.n.solomonov@gmail.com>
2025-01-07 22:10:26 +0100

    Add conform-formatter

--- lua/hliebs/plugins/lsp/mason.lua
+++ lua/hliebs/plugins/lsp/mason.lua
@@ -1,41 +1,42 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-      },
-    })
+		mason_lspconfig.setup({
+			automatic_installation = {},
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "stylua", -- lua formatter
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+			},
+		})
+	end,
 }
