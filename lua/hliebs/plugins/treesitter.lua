1d87fc3bbb8834bc4ddbea77fc7dbf888efe2152
HliebS <h.n.solomonov@gmail.com>
2025-01-15 09:16:00 +0100

    Add defaults and csharp to treesitter

--- lua/hliebs/plugins/treesitter.lua
+++ lua/hliebs/plugins/treesitter.lua
@@ -1,14 +1,26 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function() 
-      local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-      treesitter.setup({
-          ensure_installed = { "lua", "vim", "vimdoc" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+		treesitter.setup({
+			modules = {},
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline", -- Defaults
+				"c_sharp",
+			},
+			auto_install = false,
+			ignore_install = {},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
