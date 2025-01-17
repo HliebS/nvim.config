3dcb5b40a6913764127b4285e9509e954c25e6aa
HliebS <h.n.solomonov@gmail.com>
2025-01-17 16:32:36 +0100

    Rename colourscheme plugin initialization file

---
+++ lua/hliebs/plugins/colourscheme.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		style = "moon",
+	},
+	styles = {
+		sidebars = "dark",
+		floats = "dark",
+	},
+	config = function()
+		vim.cmd("colorscheme tokyonight")
+	end,
+}
