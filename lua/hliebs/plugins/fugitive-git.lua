edb5dc1dfb17c7821871c0f7e7b303fe55421cfe
HliebS <h.n.solomonov@gmail.com>
2025-03-19 17:07:48 +0100

    feat: Add keymap for git integration

--- lua/hliebs/plugins/fugitive-git.lua
+++ lua/hliebs/plugins/fugitive-git.lua
@@ -1,3 +1,17 @@
 return {
 	"tpope/vim-fugitive",
+	config = function()
+		local function toggle_fugitive()
+			local fugitive_window = vim.fn.bufwinnr("fugitive://")
+			if fugitive_window ~= -1 then
+				-- If the fugitive window is open, close it
+				vim.cmd("q")
+			else
+				-- If the fugitive window is not open, open it
+				vim.cmd("Git")
+			end
+		end
+
+		vim.keymap.set("n", "<leader>gi", toggle_fugitive, { desc = "Toggle Git" })
+	end,
 }
