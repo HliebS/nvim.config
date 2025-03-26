c97346d114dd6589d5a5322746552127f1eea9fb
HliebS <h.n.solomonov@gmail.com>
2025-03-26 11:29:04 +0100

    feat!: Add remap for hjkl to jhkl

--- lua/hliebs/core/keymaps.lua
+++ lua/hliebs/core/keymaps.lua
@@ -1,13 +1,15 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
+keymap.set({ "n", "v", "o" }, "j", "h")
+keymap.set({ "n", "v", "o" }, "h", "j")
 
 keymap.set("n", "<leader>pv", "<cmd>NvimTreeToggle<CR>")
 
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "H", ":m '>+1<CR>gv=gv")
 
-keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "H", "mzJ`z")
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "n", "nzzzv")
